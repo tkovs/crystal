86424eecb203f9899b0963cd3f3c5dd50c6f085d
tkovs <tkovs@outlook.com>
2016-10-06 21:16:03 -0300

    Alterando nome e projeto

---
+++ README.md
@@ -0,0 +1,10 @@
+## crystals
+---
+
+Sites e provas com simplicidade e eficiência!
+
+Queremos oferecer a você a chance de resolver provas sobre os mais diversos assuntos e exibir resultados para que você melhore naquilo que tem mais dificuldade. Entre nossos objetivos, estão:
+
+- Proporcionar uma ferramenta fácil para resolução de provas
+- Alta organização, categorizando cada questão
+- Análise de resultados de provas
--- stones/wsgi.py
+++ stones/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for crystals project.
+WSGI config for stones project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crystals.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "stones.settings")
 
 application = get_wsgi_application()
