a0a9b6723fcc2f9c17b360fc073d0b5a4d337228
tkovs <victor.rdg@hotmail.com>
2016-10-11 16:41:21 -0300

    Criação de página index com uso de templates

--- crystal/views.py
+++ crystal/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+from django.http     import HttpResponse
+from django.template import loader
 
 def index(request):
-	return HttpResponse('index')
+	template = loader.get_template('crystal/index.html')
+	context = {'title': 'Crystal'}
+
+	return HttpResponse(template.render(context, request))
